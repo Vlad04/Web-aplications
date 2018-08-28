9f2b0f1dc1970d537002c779643ac183f35e4915
Vlad04 <vladiir.rod96@gmail.com>
2018-08-27 20:15:03 -0500

    Lab1 Done

--- go/src/Lab1/server.go
+++ go/src/Lab1/server.go
@@ -4,13 +4,49 @@ import
   "fmt"
   "log"
   "net/http"
+  "compress/gzip"
+  "io"
+  "strings"
 )
 
+type gzipResponseWriter struct {
+	io.Writer
+	http.ResponseWriter
+}
+
+func (w gzipResponseWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
+}
+
+func makeGzipHandler(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			fn(w, r)
+			return
+		}
+		w.Header().Set("Content-Encoding", "gzip")
+		gz := gzip.NewWriter(w)
+		defer gz.Close()
+		gzr := gzipResponseWriter{Writer: gz, ResponseWriter: w}
+		fn(gzr, r)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("This is a test."))
+}
+
 func darMensaje(w http.ResponseWriter, r *http.Request) {
+  	w.Header().Set("Content-Type", "text/plain")
   
-  /*w.WriteHeader(http.StatusNotImplemented)
-  w.Write([]byte("501 Not Implementes "))*/
-  w.Header().Set("http.StatusNotImplemented", "501 Not Implementes ")
+    w.WriteHeader(http.StatusNotImplemented)
+  	w.Write([]byte("501 Not Implementes"))
+    
+    /*w.WriteHeader(http.StatusCreated)
+  	w.Write([]byte("201 Created "))*/
+
   fmt.Fprintf(w, "Laboratory #1")
 
 
@@ -18,19 +54,16 @@ func darMensaje(w http.ResponseWriter, r *http.Request) {
 
 
 func miNombre(w http.ResponseWriter, r *http.Request) {
-
-  w.Header().Set("Header2", "CREATED: 201")
-  /*w.WriteHeader(http.StatusOK)
-  w.Write([]byte("200 OK "))*/
-      w.Header().Set("http.StatusOK", "200 OK ")
   fmt.Fprintf(w, "Vladimir Rodriguez Bahena")
 }
 
 
 func main() {
   http.HandleFunc("/", darMensaje)
-  http.HandleFunc("/home/vladimir/Web_aplications/go/src/Lab1", miNombre)
+  //http.HandleFunc("/home/vladimir/Web_aplications/go/src/Lab1", miNombre)
+  http.HandleFunc("/zip", makeGzipHandler(handler))
   err := http.ListenAndServe("localhost"+":"+"8080", nil)
+  //http.ListenAndServe(":8080", makeGzipHandler(darMensaje))
   if err != nil {
     log.Fatal("error en el servidor : ", err)
     return
